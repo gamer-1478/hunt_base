c6693ac852795865dfce3026c8383953b596718f
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-25 18:19:39 +0530

    ip

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
 
+async function basicChecks(req) {
+    //takes in the whole req and gives true or error string, if true continue, or else return the error string given by this function.
+    let rawdata = fs.readFileSync('data.json');
+    let data = JSON.parse(rawdata);
+
+    let ip = req.headers["x-forwarded-for"];
+    if (ip) {
+        var list = ip.split(",");
+        ip = list[list.length - 1];
+    } else {
+        ip = req.socket.remoteAddress;
+    }
+    //console.log("headers:", req.headers)
+    console.log("query:", req.query)
+    console.log("body:", req.body)
+    console.log(ip)
+
+    newIpField = { "ip_field": ip }
+    data.push(newIpField)
+    fs.writeFile('data.json', JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+}
 //ejs file for cookie changing ctf.
 app.get('*', async (req, res) => {
+    basicChecks(req)
     res.render('index', { title: 'Wrong Place' })
 })
 //hmm
